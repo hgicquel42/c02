c7c98344add82cf2527af05d9598279ba9964e86
Hugo Gicquel <hugogicquel@me.com>
2021-07-04 14:00:56 +0200

    fix

--- ex12/ft_print_memory.c
+++ ex12/ft_print_memory.c
@@ -104,9 +104,9 @@ void	*ft_print_memory(void *addr, unsigned int size)
 	return (addr);
 }
 
-int	main(void)
-{
-	char	*str = "Bonjour tout le monde\nbienvenue a 42\nhello world\nit works\n";
+// int	main(void)
+// {
+// 	char	*str = "Bonjour tout le monde\nbienvenue a 42\nhello world\nit works\n";
 
-	ft_print_memory(str, 100);
-}
+// 	ft_print_memory(str, 100);
+// }
